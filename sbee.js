58ccf071f2f66b9fd0fd884c9abaa25091971b8e
Avinash Varna <avinashvarna@users.noreply.github.com>
2023-05-25 20:53:07 +0000

    Handle accidental page refresh

--- sbee.js
+++ sbee.js
@@ -4,19 +4,22 @@ var foundWordsField = document.getElementById("foundWords");
 var score = 0;
 var foundWords = new Set();
 
-function update() {
-    const words = inputWords.value.trim().split(/\s+/g);
-    // console.log(words);
+function init() {
+    if(localStorage.getItem("sbee.date") == new Date().toLocaleDateString()) {
+        wordList = localStorage.getItem("sbee.wordList");
+        update(wordList);
+    }
+}
+
+function update(wordList) {
+    if(!wordList) wordList = inputWords.value;
+    const words = wordList.trim().split(/\s+/g);
     inputWords.value = "";
     for (let i=0; i < words.length; i++) {
-        let currentWord = words[i];
+        let currentWord = words[i].toLowerCase();
         if(!foundWords.has(currentWord)) {
             score += currentWord.length == 4 ? 1 : currentWord.length;
-            let uniqueChars = new Set();
-            for(let j=0; j < currentWord.length; j++) {
-                uniqueChars.add(currentWord[j].toLowerCase());
-            }
-            // console.log(uniqueChars);
+            let uniqueChars = new Set(currentWord);
             if(uniqueChars.size == 7) {
                 score += 7;
                 foundWordsField.innerHTML += " <b>" + currentWord + "</b>";
@@ -27,4 +30,6 @@ function update() {
         foundWords.add(currentWord);
     }
     scoreField.innerHTML = "Score: " + score;
+    localStorage.setItem("sbee.date", new Date().toLocaleDateString());
+    localStorage.setItem("sbee.wordList", [...foundWords].join(" "));
 }
